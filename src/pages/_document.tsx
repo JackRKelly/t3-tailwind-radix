2b7a003412bcee3ca5c18c8fcc83ca6809f6c678
Jack Kelly <kcjackkelly@gmail.com>
2022-10-24 00:48:01 -0500

    next document functional component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,18 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
+const Document = () => {
+	return (
+		<Html className="bg-app text-type">
+			<Head>
+				<meta name="theme-color" content="#111827" />
+				<script src="/static/theme.js" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
 
-	render() {
-		return (
-			<Html className="bg-app text-type">
-				<Head>
-					<meta name="theme-color" content="#111827" />
-					<script src="/static/theme.js" />
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
-}
-
-export default MyDocument;
+export default Document;
