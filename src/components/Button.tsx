44b8081d1fc2750398d973499d51a5d075c5ed71
Jack Kelly <kcjackkelly@gmail.com>
2022-10-24 23:21:54 -0500

    remove unused button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 type Props = React.ComponentProps<"button">;
 
-const _Button = tw.button`inline-flex select-none bg-primitive-faint items-center justify-center rounded-md px-4 py-2 text-sm font-medium border border-primitive-edge text-primitive-type hover:bg-primitive focus:outline-none focus-visible:ring focus-visible:ring-highlight group first-line:radix-state-open:bg-primitive-bold first-letter:radix-state-instant-open:bg-primitive-bold radix-state-delayed-open:bg-primitive-bold transition-button focus-visible:border-transparent`;
+const _Button = tw.button`inline-flex select-none bg-primitive-faint items-center justify-center rounded-md px-4 py-2 text-sm font-medium border border-primitive-edge text-primitive-type hover:bg-primitive focus:outline-none focus-visible:ring focus-visible:ring-highlight group transition-button focus-visible:border-transparent`;
 
 const Button = React.forwardRef<HTMLButtonElement, Props>(({ children, ...props }, ref) => {
 	return (
