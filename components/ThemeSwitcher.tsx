b772cce51f6a03bf74cd7ab2dd95f0fc84b1d73a
Jack Kelly <kcjackkelly@gmail.com>
2022-10-26 00:21:41 -0500

    clean up theme switcher

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -50,12 +50,11 @@ const Arrow = tw(
 	DropdownMenuPrimitive.DropdownMenuArrow
 )`fill-current text-primitive-edge flex w-2 h-1`;
 
-interface Props {
-	side?: DropdownMenuPrimitive.DropdownMenuContentProps["side"];
-	sideOffset?: DropdownMenuPrimitive.DropdownMenuContentProps["sideOffset"];
-	align?: DropdownMenuPrimitive.DropdownMenuContentProps["align"];
-	alignOffset?: DropdownMenuPrimitive.DropdownMenuContentProps["alignOffset"];
-}
+interface Props
+	extends Pick<
+		DropdownMenuPrimitive.DropdownMenuContentProps,
+		"side" | "sideOffset" | "align" | "alignOffset"
+	> {}
 
 const ThemeSwitcher = (props: Props) => {
 	const [preferredTheme, setPreferredTheme] = useState<null | ThemeStyle>(null);
@@ -82,52 +81,44 @@ const ThemeSwitcher = (props: Props) => {
 	const { align, alignOffset, side, sideOffset = 4 } = props;
 
 	return (
-		<div className="relative inline-block text-left">
-			<DropdownMenuPrimitive.Root>
-				<Trigger>
-					{(() => {
-						switch (preferredTheme) {
-							case "light":
-								return <SunIcon className="h-5 w-5 text-primitive-type" />;
-							case "dark":
-								return <MoonIcon className="h-5 w-5 text-primitive-type" />;
-							default:
-								return <Half2Icon className="h-5 w-5 text-primitive-type" />;
-						}
-					})()}
-				</Trigger>
+		<DropdownMenuPrimitive.Root>
+			<Trigger>
+				{(() => {
+					switch (preferredTheme) {
+						case "light":
+							return <SunIcon className="h-5 w-5 text-primitive-type" />;
+						case "dark":
+							return <MoonIcon className="h-5 w-5 text-primitive-type" />;
+						default:
+							return <Half2Icon className="h-5 w-5 text-primitive-type" />;
+					}
+				})()}
+			</Trigger>
 
-				<DropdownMenuPrimitive.Portal>
-					<Content
-						sideOffset={sideOffset}
-						side={side}
-						alignOffset={alignOffset}
-						align={align}
-						forceMount
-					>
-						<ContentInner>
-							<Arrow />
-							{themes.map(({ key, label, icon }, i) => {
-								return (
-									<Item
-										key={`theme-${i}`}
-										onClick={() => {
-											(window as any).__setPreferredTheme(key);
-											setPreferredTheme(key);
-										}}
-									>
-										{React.cloneElement(icon, {
-											className: "w-5 h-5 mr-2 text-primitive-type-extra-faint"
-										})}
-										<Label>{label}</Label>
-									</Item>
-								);
-							})}
-						</ContentInner>
-					</Content>
-				</DropdownMenuPrimitive.Portal>
-			</DropdownMenuPrimitive.Root>
-		</div>
+			<DropdownMenuPrimitive.Portal>
+				<Content {...{ align, alignOffset, side, sideOffset }} forceMount>
+					<ContentInner>
+						<Arrow />
+						{themes.map(({ key, label, icon }, i) => {
+							return (
+								<Item
+									key={`theme-${i}`}
+									onClick={() => {
+										(window as any).__setPreferredTheme(key);
+										setPreferredTheme(key);
+									}}
+								>
+									{React.cloneElement(icon, {
+										className: "w-5 h-5 mr-2 text-primitive-type-extra-faint"
+									})}
+									<Label>{label}</Label>
+								</Item>
+							);
+						})}
+					</ContentInner>
+				</Content>
+			</DropdownMenuPrimitive.Portal>
+		</DropdownMenuPrimitive.Root>
 	);
 };
 
