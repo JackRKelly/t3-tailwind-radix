0680f62bb2fd70a3ec1d12102a4aaae828cf2109
Jack Kelly <kcjackkelly@gmail.com>
2023-04-19 23:18:14 -0500

    remove env from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 // @ts-check
-// organize-imports-ignore
-import { env } from "./utils/env.mjs";
-
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
